b0b9efda96b6e27840ee26341283d7921a2ed7aa
kuyucucaner <tahacanerkuyucu@gmail.com>
2024-09-09 15:17:48 +0300

    package v3

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,37 @@
 const express = require('express');
-const dotenv = require('dotenv');
+const path = require('path');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+require('dotenv').config();
 
 
-dotenv.config();
+// Uygulama oluşturma
 const app = express();
 
-// Middleware
+// Orta katmanlar (middlewares)
 app.use(express.json());
+app.use(cors());
 
+
+app.use(helmet());
+
+app.use(morgan('tiny'));
+
+
+// Frontend build klasörünü backend üzerinden sunma
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+// Diğer tüm istekler için index.html dosyasını sunma
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Bir hata oluştu!');
+});
+
+// Port Ayarlama ve Sunucuyu Başlatma
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Sunucu ${PORT} portunda çalışıyor`));
